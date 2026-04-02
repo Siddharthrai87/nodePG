0ca7fdba36d5be4cc34c85be2f33d1f4dce2cab9
Siddharth Rai <raisk11112000@gmail.com>
2026-04-02 17:56:37 +0530

    file handling

---
+++ hello-world/file.js
@@ -0,0 +1,22 @@
+const { error } = require("console");
+const fs=require("fs");
+
+// fs.writeFileSync('./text.txt',"hey there");
+
+// const result=fs.readFileSync("./text.txt","utf-8");
+// console.log(result);
+
+// fs.readFile("./text.txt","utf-8",(error,result)=>{
+//     if(error)console.log(error);
+//     else console.log(result);
+// })
+
+
+// fs.writeFile("test.txt","hello",(err)=>{
+//     if(err)console.log(err);
+// })
+
+// fs.unlinkSync("./test.txt");
+
+console.log(fs.statSync("./text.txt"));
+fs.mkdirSync("hell/a/b",{recursive:true});
