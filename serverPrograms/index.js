5ceb25b3606a8a05ae1752ebf0fd550ff94d470c
Siddharth Rai <raisk11112000@gmail.com>
2026-04-05 23:52:26 +0530

    httpMethods and Express

--- serverPrograms/index.js
+++ serverPrograms/index.js
@@ -1,15 +1,42 @@
-const http=require("http");
-const fs=require("fs");
-
-const myServer=http.createServer((req,res)=>{
-    const timestamp=Date.now();
-    const date=new Date(timestamp);
-    const log=`${date.toString()} New req received at ${req.url} \n`;
-    fs.appendFile('log.txt',log,(err,result)=>{
-        res.end("Hello from server");
-    })
+// line 2-12 shows server creation and calling by express where as line 16-42 shows creation and calling by node
+const express=require("express");
+const app=express();
+app.get('/',(req,res)=>{
+    res.send("Welcome to Home Page");
 })
-
-myServer.listen(8000,()=>{
+app.get('/about',(req,res)=>{
+    res.send("Welcome to About Page");
+})
+app.listen(8000,()=>{
     console.log("Server started");
 })
+
+
+
+// const http=require("http");
+// const fs=require("fs");
+// const url=require("url");
+// function myHandler(req,res){
+//     if(req.url==="/favicon.ico")return res.end();
+//     const timestamp=Date.now();
+//     const date=new Date(timestamp);
+//     const log=`${date.toString()} ${req.method}New req received at ${req.url} \n`;
+//     const myUrl=url.parse(req.url,true);
+//     console.log(myUrl);
+//     fs.appendFile('log.txt',log,(err,result)=>{
+//         switch(myUrl.pathname){
+//             case "/":res.end("Homepage");
+//                     break;
+//             case "/about":
+//                     const userName=myUrl.query.name;
+//                     res.end(`Hi ${userName}`);
+//                     break;
+//             default:res.end("404 Not Found");
+//                     break;
+//         }
+//     })
+// }
+// const myServer=http.createServer(myHandler);
+// myServer.listen(8000,()=>{
+//     console.log("Server started");
+// })
